c472a05177c20b87d72775fef497a257933bba1a
Gerhardus van Biljon <vanbiljongerhardus@gmail.com>
2025-07-17 02:16:50 +0200

    Add files via upload

---
+++ src/poses/index.ts
@@ -0,0 +1,4 @@
+// src/poses/index.ts
+export { catPoses } from "./sitPose";
+// If you had other categories like walkPose or stretchPose, you’d add them here:
+// export { walkPoses } from "./walkPose";
