dafde2f70c21b2266dbf6ac2a187cc0e585617f0
Gerhardus van Biljon <vanbiljongerhardus@gmail.com>
2025-07-17 02:16:35 +0200

    Delete src/poses directory

--- src/poses/index.ts
+++
@@ -1,4 +0,0 @@
-// src/poses/index.ts
-export { catPoses } from "./sitPose";
-// If you had other categories like walkPose or stretchPose, you’d add them here:
-// export { walkPoses } from "./walkPose";
