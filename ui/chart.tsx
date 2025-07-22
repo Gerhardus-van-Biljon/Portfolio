64d50f0234dd2a5e5917dbe578152a22cf7f5c1b
Gerhardus van Biljon <vanbiljongerhardus@gmail.com>
2025-07-22 17:50:37 +0200

    Update chart.tsx

--- ui/chart.tsx
+++ ui/chart.tsx
@@ -1,4 +1,4 @@
-"use client";
+/*"use client";
 
 import * as React from "react";
 import * as RechartsPrimitive from "recharts";
@@ -350,4 +350,4 @@ export {
   ChartLegend,
   ChartLegendContent,
   ChartStyle,
-};
+};*/
